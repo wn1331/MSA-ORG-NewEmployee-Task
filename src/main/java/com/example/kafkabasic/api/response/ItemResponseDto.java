87bd71e0e6925036c09304fb904379462939ecbd
whdcks420 <whdcks420@gmail.com>
2023-05-23 14:57:00 +0900

    아이템 컨트롤러 및 초기 화면 서비스 기능 추가

---
+++ src/main/java/com/example/kafkabasic/api/response/ItemResponseDto.java
@@ -0,0 +1,4 @@
+package com.example.kafkabasic.api.response;
+
+public record ItemResponseDto(String itemName, int stockQuantity) {
+}
--- src/main/java/com/example/kafkabasic/infrastructure/init/InitDb.java
+++ src/main/java/com/example/kafkabasic/infrastructure/init/InitDb.java
@@ -17,14 +17,14 @@ public class InitDb {
         itemInitDb();
     }
 
-    public void itemInitDb() {
+    private void itemInitDb() {
         createItem();
     }
 
     private void createItem() {
-        CreateItemRequestDto request1 = new CreateItemRequestDto("product1", 1000000, 10);
-        CreateItemRequestDto request2 = new CreateItemRequestDto("product2", 1000000, 5);
-        CreateItemRequestDto request3 = new CreateItemRequestDto("product3", 1000000, 0);
+        CreateItemRequestDto request1 = new CreateItemRequestDto("스프링", 50000, 10);
+        CreateItemRequestDto request2 = new CreateItemRequestDto("카프카", 30000, 5);
+        CreateItemRequestDto request3 = new CreateItemRequestDto("리액트", 10000, 1);
         itemService.createItem(request1);
         itemService.createItem(request2);
         itemService.createItem(request3);
