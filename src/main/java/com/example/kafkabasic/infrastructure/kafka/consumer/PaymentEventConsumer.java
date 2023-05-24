d77d69d969d11339b5d1ec391cd9359fc6905529
whdcks420 <whdcks420@gmail.com>
2023-05-24 10:37:39 +0900

    node-exporter docker-compose 추가

--- src/main/java/com/example/kafkabasic/infrastructure/kafka/consumer/PaymentEventConsumer.java
+++ src/main/java/com/example/kafkabasic/infrastructure/kafka/consumer/PaymentEventConsumer.java
@@ -2,16 +2,10 @@ package com.example.kafkabasic.infrastructure.kafka.consumer;
 
 import com.example.kafkabasic.application.event.OrderPaymentEvent;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
-
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 
 @Component
 @Slf4j
@@ -23,12 +17,10 @@ public class PaymentEventConsumer {
 
         try {
             ObjectMapper objectMapper = new ObjectMapper();
-
             String ope = objectMapper.readValue(orderPaymentEvent, String.class);
             OrderPaymentEvent event = objectMapper.readValue(ope,OrderPaymentEvent.class);
             System.out.println(event.toString());
         } catch (JsonProcessingException e) {
-            // 역직렬화 오류 처리 로직을 추가합니다.
             log.error("Error deserializing OrderPaymentEvent: {}", e.getMessage());
         }
     }
